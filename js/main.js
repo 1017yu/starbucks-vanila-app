a2081547f329596629bc695fc08beeb6b4751cdc
Kled <1017yu@daum.net>
2022-01-11 18:29:39 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,9 +8,29 @@ searchEl.addEventListener('click', function () {
 searchInputEl.addEventListener('focus', function () {
   searchEl.classList.add('focused');
   searchInputEl.setAttribute('placeholder', '통합검색');
-})
+});
 
 searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
-})
+});
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function (){
+  if (window.scrollY > 500) {
+    // 배지 badge 숨기기
+    // gsap.to(요소, 지속시간(s), {옵션})
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    // 배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+// _.throttle(함수, 시간(ms))
