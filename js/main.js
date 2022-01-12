91b7c4edace5599165fb71349fa8ce6c402f71b1
Kled <1017yu@daum.net>
2022-01-12 17:45:18 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
+
+// Search
 searchEl.addEventListener('click', function () {
   searchInputEl.focus();
 });
@@ -15,6 +17,7 @@ searchInputEl.addEventListener('blur', function () {
   searchInputEl.setAttribute('placeholder', '');
 });
 
+// Badge
 const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(function (){
@@ -34,3 +37,25 @@ window.addEventListener('scroll', _.throttle(function (){
   }
 }, 300));
 // _.throttle(함수, 시간(ms))
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    opacity: 1,
+    delay: (index + 1) * .7
+  })
+});
+
+// SWIPER
+// new Swiper(선택자, 옵션)
+var swiper = new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      slidesPerView : '1'
+
+    },
+    autoplay: true,
+    loop: true,
+});
